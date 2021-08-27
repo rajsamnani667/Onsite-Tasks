a8535a63a1fe3248d8ab27b58d06b8887847ff8a
rajsamnani667 <78310817+rajsamnani667@users.noreply.github.com>
2021-08-27 13:28:04 +1100

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if(isset($_POST['submit'])){
              function cap(){
 
                  var alpha=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','0','P','Q','R','S','T','U','V','W','X','Y','Z'
-                            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+                            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'];
                  var a=alpha[Math.floor(Math.random()*62)];
                  var b=alpha[Math.floor(Math.random()*62)];
                  var c=alpha[Math.floor(Math.random()*62)];
